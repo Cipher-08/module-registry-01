41f3f22a4ffbe8e0bce5f364b3bb051beee97da0
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-04-16 14:36:41 +0530

    Create variables.tf

---
+++ module/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  type        = string
+  description = "The AWS region where the resources will be created"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket to be created"
+  default     = "aws-env0-bucket-test-4398438-1"
+}
+
+variable "bucket_tags" {
+  type        = map(string)
+  description = "The tags to be applied to the S3 bucket"
+  default = {
+    Name        = "Nameofthebucket"
+    Environment = "Production"
+  }
+}
